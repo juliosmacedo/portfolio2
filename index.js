bac1a4058358fd6faf40f74258867a156618f960
Julio Macedo <111392972+juliosmacedo@users.noreply.github.com>
2022-11-19 14:31:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
+import initScrollReveal from "./scripts/scrollReveal.js";
+import initTiltEffect from "./scripts/tiltAnimation.js";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
